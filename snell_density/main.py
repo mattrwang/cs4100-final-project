8fb6fd13a16f621a5c5fbeeab46283d2167db88f
Matthew Wang <wang.mat@northeastern.edu>
2024-03-26 14:13:38 -0400

    fixed path

--- snell_density/main.py
+++ snell_density/main.py
@@ -1,8 +1,8 @@
 from FILE_READER import read_files
 from DATE_PARSER import tabulate_days
 
-lines = read_files("Snell Data")
+lines = read_files("snell_density/Snell Data")
 table = tabulate_days(lines)
-#print(len(lines), " items") #1579308
-#print(table)
-#print(sum(table[2])) # expect 436 entries on january 3rd
+# print(len(lines), " items") #1579308
+# print(table)
+# print(sum(table[2])) # expect 436 entries on january 3rd

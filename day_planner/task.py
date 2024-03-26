a10728817e2e84472abb0fe31904accbc0c2dbfb
gellerjulia <74266997+gellerjulia@users.noreply.github.com>
2024-03-26 08:17:38 -0400

    asdf

--- day_planner/task.py
+++ day_planner/task.py
@@ -1,14 +1,14 @@
 """ Task.py
 Defines Task class which is one activity a user needs to complete.
 """
-from typing import List, Tuple, TypeVar
+from typing import List, TypeVar
 
 class Task:
-    def __init__(self, pe: int, total_hours: float, location: str = None, fixed_times: List[List[TypeVar]] = None, mode=None):
+    def __init__(self, pe: int, total_hours: float, location: str = None, fixed_time: List[TypeVar] = None, mode=None):
         self.pe = pe # perceived energy taken per hour of working on the task, from 1-10 (1 is a very easy task, 10 is a very draining task)
         self.total_hours = total_hours # total number of hours user needs to work on this task in the week (decimal portion must be in quarters)
         self.location = location # address of the task if the task needs to be completed in a certain location
-        self.fixed_times = fixed_times # List[List[day: str, start_time: int, end_time:int, mode:str]] where day is day of the week task needs 
+        self.fixed_time = fixed_time # List[day: str, start_time: float, end_time: float] where day is day of the week task needs 
                                        # to be completed and start and end times are times task needs to be comlplted (military time int),
         self.mode = mode # mode is mode of transportation one of walking, transit, driving, bicycling
 

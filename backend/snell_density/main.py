8fe0fc2b0e1c8eb14f20e22a4f6b7e0ae2ad7811
Matthew Wang <wang.mat@northeastern.edu>
2024-03-27 17:55:24 -0400

    fixed path

--- backend/snell_density/main.py
+++ backend/snell_density/main.py
@@ -1,7 +1,7 @@
 from FILE_READER import read_files
 from DATE_PARSER import tabulate_days
 
-lines = read_files("snell_density/Snell Data")
+lines = read_files("backend/snell_density/Snell Data")
 table = tabulate_days(lines)
 # print(len(lines), " items") #1579308
 # print(table)
